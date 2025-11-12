f1e11b5ef9bd4a4fffba560a25903468b9b1997a
HtoE <hyosilver0419@duksung.ac.kr>
2025-11-12 12:28:58 +0900

    Update in_po_05.py

--- work5/in_po_05.py
+++ work5/in_po_05.py
@@ -10,4 +10,23 @@ class DeliveryFood(Food):
         super().__init__(name,price)
         self.delivery_fee=delivery_fee
     def __str__(self):
-        return f'메뉴:{self.name},가격:{self.price + self.delivery_fee}(배달비 포함)'   
+        return f'메뉴:{self.name},총 가격:{self.price + self.delivery_fee}(배달비 포함)'   
+
+class Order:
+    def __init__(self):
+        self.menu_list=[]
+    def add_food(self,menu):
+        self.menu_list.append(menu)
+    def show_order(self):
+        print('====주문 내역===')
+        for i in self.menu_list:
+            print(i.__str__())
+
+f1=Food('김밥',3000)
+f2=DeliveryFood('짜장면',6000,2000)
+
+order=Order()
+order.add_food(f1)
+order.add_food(f2)
+order.show_order()
+        
